d90639a36ee3b05f96062d9f7afa9e72333e5b32
evildead302 <jonepeter672@gmail.com>
2026-06-14 08:45:56 +0500

    Change year format in getTimestampString method

--- api/base61.js
+++ api/base61.js
@@ -52,7 +52,7 @@ class Base61 {
   
   // Generate timestamp string in YYMMDDHHMMSSS format
   static getTimestampString(date = new Date()) {
-    const year = date.getFullYear().toString().slice(-2);
+    const year = date.getFullYear().toString();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
     const hours = String(date.getHours()).padStart(2, '0');
